9e5648c7b1b9263c1def4ab9e6830c2af8e4d54f
qarq90 <scythethroughtheheart05@gmail.com>
2024-03-18 19:32:35 +0000

    SnX Completion - 18/03/2024

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {useEffect} from "react";
 import {motion} from "framer-motion";
 import {scaleUp} from "@/styles/styledAnimations";
 import HomeTitle from "@/components/home/HomeTitle";
